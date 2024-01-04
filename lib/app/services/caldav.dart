5d77f900354bea0070a832f6565cb67f5566f47e
mario meltzow <dev@meltzow.net>
2024-01-04 10:58:09 +0100

    using functions in caldav lib

--- lib/app/services/caldav.dart
+++ lib/app/services/caldav.dart
@@ -1,5 +1,4 @@
 import 'package:caldav_client/caldav_client.dart';
-import 'package:caldav_client/src/utils.dart';
 
 class CalDav {
   Future<void> request() async {
@@ -11,35 +10,18 @@ class CalDav {
     var principalPath = await client.getPrincipal('/remote.php/dav');
     var calHomesetPath = await client.getCalendarHomeSet(principalPath);
 
-    var initialSyncResult = await client.getCalendars(calHomesetPath);
+    var allCaledendars = await client.getCalendars(calHomesetPath);
 
     var calendars = <String>[];
 
     // Print calendars and save calendars path
-    for (var response in initialSyncResult.multistatus!.responses) {
-      print('PATH: ${response.href}');
-
-      if (response.status.contains("200")) {
-        var displayname = response.propstat?.prop['displayname'];
-        var ctag = response.propstat?.prop['getctag'];
-
-        if (displayname != null && ctag != null) {
-          print('CALENDAR: $displayname');
-          print('CTAG: $ctag');
-
-          calendars.add(response.href);
-        } else {
-          print('This collection is not a calendar');
-        }
-      } else {
-        print('Bad prop status');
-      }
+    for (var cal in allCaledendars) {
+      print('PATH: ${cal.displayName} - ${cal.supportedCalendarComponentSet}');
     }
 
     // Print calendar objects info
     if (calendars.isNotEmpty) {
       await getEvents(client, calendars);
-      await generateEvent(client, calendars);
     }
   }
 
@@ -55,34 +37,5 @@ class CalDav {
       }
       print('Bad prop status');
     }
-  }
-
-  Future<void> generateEvent(
-      CalDavClient client, List<String> calendars) async {
-    var calendar = '''
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//PYVOBJECT//NONSGML Version 1//EN
-BEGIN:VEVENT
-UID:test@example.com
-DTSTART;VALUE=DATE:20190306
-CLASS:PRIVATE
-DESCRIPTION:Arman and Adrian released their SRT-file parser library for Dar
- t
-DTSTAMP;X-VOBJ-FLOATINGTIME-ALLOWED=TRUE:20190306T000000
-LOCATION:Heilbronn
-PRIORITY:0
-RRULE:FREQ=YEARLY
-STATUS:CONFIRMED
-SUMMARY:SRT-file Parser Release
-URL:https://pub.dartlang.org/packages/srt_parser
-END:VEVENT
-END:VCALENDAR''';
-
-    // Create calendar
-    var createCalResponse =
-        await client.putCal(join(calendars.first, '/example.ics'), calendar);
-
-    if (createCalResponse.statusCode == 201) print('Created');
   }
 }
